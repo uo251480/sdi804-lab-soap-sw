2e9a043d770d7f501096d235297103255cab08a8
David <uo251480@uniovi.es>
2019-05-20 20:38:39 +0200

    init

---
+++ sdi804-lab-soap-sw/src/main/java/com/uniovi/Sdi804LabSoapSwApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi804LabSoapSwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi804LabSoapSwApplication.class, args);
+	}
+
+}
